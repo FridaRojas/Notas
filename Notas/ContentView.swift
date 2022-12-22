1df27a982296ab6c6d8b53703d509dc852fc5867
Frida Rojas <frida.rojasa@alumno.buap.mx>
2022-12-22 12:50:35 -0600

    Initial Commit

---
+++ Notas/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Notas
+//
+//  Created by Frida Rojas Alarcon on 22/12/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+
+    var body: some View {
+       Text("Hola")
+    }
+
+
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
